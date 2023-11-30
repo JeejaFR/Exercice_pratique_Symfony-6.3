a71773d893fd143c587b612b0a6c547cc84339a4
JeejaFR <jeremy.defuentes@gmail.com>
2023-11-30 11:03:53 +0100

    feat: CRUD livre

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -2,17 +2,96 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Livre;
+use App\Form\LivreType;
+use App\Repository\LivreRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/livre', name: 'app.livre')]
 class LivreController extends AbstractController
 {
-    #[Route('/livre', name: 'app_livre')]
+
+    public function __construct(
+        private EntityManagerInterface $em,
+        private LivreRepository $repo,
+    )
+    {
+    }
+
+    #[Route('', name: '.index', methods:['GET'])]
     public function index(): Response
     {
-        return $this->render('livre/index.html.twig', [
-            'controller_name' => 'LivreController',
+        return $this->render("livre/index.html.twig",[
+            'livres' => $this->repo->findAll(),
         ]);
     }
+
+    #[Route('/create',name: '.create',methods: ['GET','POST'])]
+    public function create(Request $request): Response | RedirectResponse {
+        $livre = new Livre();
+
+        $form = $this->createForm(LivreType::class, $livre);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($livre);
+            $this->em->flush();
+
+            $this->addFlash('success',"Le livre à été crée avec succès");
+            return $this->redirectToRoute('app.livre.index');
+        }
+
+        return $this->render('livre/create.html.twig',[
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/update', name:'.update',methods:['GET','POST'])]
+    public function update(?Livre $livre, Request $request): Response | RedirectResponse{
+        if(!$livre instanceof Livre){
+            $this->addFlash('error',"Le livre est introuvable");
+            return $this->redirectToRoute('app.livre.index');
+        }
+
+        $form = $this->createForm(LivreType::class,$livre);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($livre);
+            $this->em->flush();
+
+            $this->addFlash('success',"Le livre à été modifié avec succès");
+            return $this->redirectToRoute('app.livre.index');
+        }
+
+        return $this->render('livre/update.html.twig', [
+            'form' => $form,
+            'livre' => $livre,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name:'.delete',methods: ['POST'])]
+    public function delete(?Livre $livre, Request $request): RedirectResponse {
+        if(!$livre instanceof Livre){
+            $this->addFlash('error','Le livre est introuvable');
+            return $this->redirectToRoute('app.livre.index');
+        }
+
+        if($this->isCsrfTokenValid('delete'. $livre->getId(), $request->request->get('token'))){
+            $this->em->remove($livre);
+            $this->em->flush();
+
+            $this->addFlash('success',"Le livre à été supprimé");
+            return $this->redirectToRoute('app.livre.index');
+        }
+
+        $this->addFlash('error',"token CSRF invalide, le livre n'a pas été supprimé");
+        return $this->redirectToRoute('app.livre.index');
+    }
 }
+
---
+++ src/Form/LivreType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use App\Entity\Auteur;
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'label' => 'Titre: ',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => "Titre du livre"
+                ] 
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'nom',
+                'multiple' => true,
+            ])
+            ->add('auteur', EntityType::class, [
+                'class' => Auteur::class,
+                'choice_label' => function ($auteur) {
+                    return $auteur->getNom() . ' ' . $auteur->getPrenom();
+                },
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
